b044b4ec88648481bbbe48c385697c54ac347400
Curtis Wang <curtis.wang10@gmail.com>
2022-11-02 13:14:14 -0700

    adding more tables to database, tried to do queries within same pool but it was not working concurrently

--- server/db/db.js
+++ server/db/db.js
@@ -5,13 +5,17 @@ const pool = new Pool({
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: 5432,
+  port: process.env.PGPORT,
   host: process.env.PGHOST,
 });
 
 pool
-  .query('CREATE TABLE IF NOT EXISTS product (id SERIAL PRIMARY KEY, name VARCHAR(155) NOT NULL, slogan VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, category VARCHAR(40) NOT NULL, default_price VARCHAR(10))')
+  .query('CREATE TABLE IF NOT EXISTS product (id SERIAL PRIMARY KEY, name VARCHAR(155) NOT NULL, slogan VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, category VARCHAR(40) NOT NULL, default_price VARCHAR(10) NOT NULL)')
   .then(res => console.log(res.rows))
-  .catch(err => console.error('Error executing query', err.stack))
+  .catch(err => console.error('Error creating product table', err.stack));
+pool
+  .query('CREATE TABLE IF NOT EXISTS related (id SERIAL PRIMARY KEY, current_product_id INT REFERENCES product (id), related_product_id INT)')
+  .then(res => console.log(res.rows))
+  .catch(err => console.error('Error creating related table', err.stack));
 
 module.exports = { pool };
