758d1be1abc0b148239f4cdf0e02b87257e34589
Curtis Wang <curtis.wang10@gmail.com>
2022-11-03 18:24:28 -0700

    finished copying data from csv into products tables, started outlining flow of data through routes, controllers, and the models

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,17 +2,34 @@ const models = require('../models');
 
 const products = {
   getRelated: (req, res) => {
-    // models.getRelated()
-    res.status(200).send("get related products")
+    let productID = req.query.id;
+    models.getRelated(productID, (err, related) => {
+      if (err) {
+        res.status(404).end();
+      }
+      res.status(200).json(related)
+    })
   },
   getOne: (req, res) => {
-    res.status(200).send('get one product')
+    let productID = req.query.id;
+    models.getProduct(productID, (err, product) => {
+      if (err) {
+        res.status(404).end();
+      }
+      res.status(200).json(product)
+    })
   },
   getAll: (req, res) => {
     res.status(200).send('get all products')
   },
   getStyles: (req, res) => {
-    res.status(200).send('get product styles')
+    let productID = req.query.id;
+    models.getStyles(productID, (err, styles) => {
+      if (err) {
+        res.status(404).end();
+      }
+      res.status(200).json(styles)
+    })
   }
 }
 
