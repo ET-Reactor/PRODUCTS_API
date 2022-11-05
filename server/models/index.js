88095d63f5566b10961253f59dde577539597bb5
Curtis Wang <curtis.wang10@gmail.com>
2022-11-04 18:17:39 -0700

    nested queries became too complex trying to have everything finish before calling the callback so I have switched approaches to try using combination of joins and complex queries. need to keep working on this

--- server/models/index.js
+++ server/models/index.js
@@ -37,17 +37,84 @@ module.exports = {
   },
   getStyles: async (productID, callback) => {
     try {
-      let skusArr = [];
-      const stylesResult = await pool.query("SELECT * FROM styles WHERE productId=$1", [productID]);
-      stylesResult.rows.forEach(row => {
-        let styleID = row.id;
-        const photosResult = pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
-          .then((response) => {
-            row.photos = (response.rows);
-            console.log('should have photos added', row);
-          })
-          .catch((err) => console.log('Error selecting photos'));
-      });
+      const stylesResult = await pool.query("SELECT styles.*, jsonb_agg(to_jsonb(photos) - 'id' - 'styleid') AS photos FROM styles LEFT JOIN photos ON photos.styleID=styles.id WHERE productId=$1 GROUP BY styles.id", [productID])
+      console.log(stylesResult.rows);
+      // stylesResult.rows.forEach((row, i) => {
+      //   delete row.productid;
+      //   let styleID = row.id;
+      //   pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
+      //     .then(photos => {
+      //       row.photos = photos.rows;
+
+      //       pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
+      //         .then(skus => {
+      //           row.skus = {};
+      //           for (key in skus.rows) {
+      //             row.skus[skus.rows[key].id] = {};
+      //             row.skus[skus.rows[key].id].size = skus.rows[key].size;
+      //             row.skus[skus.rows[key].id].quantity = skus.rows[key].quantity;
+      //           }
+      //           console.log(stylesResult.rows);
+      //           // if (i === stylesResult.rows.length - 1) {
+      //           //   callback(null, stylesResult.rows);
+      //           // }
+      //         })
+      //     })
+      // })
+
+      // stylesResult.rows.forEach(async row => {
+      //   let styleID = row.id;
+      //   const photosResult = await pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
+      //     .then(async (response) => {
+      //       row.photos = (response.rows);
+      //     })
+      //     // .then(() => console.log('pls', styles))
+      //     .catch((err) => console.log('Error selecting photos'));
+      //   const skusResult = await pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
+      //     .then((response) => {
+      //       // console.log('skus result', response.rows);
+      //       row.skus = {};
+      //       for (key in response.rows) {
+      //         row.skus[response.rows[key].id] = {};
+      //         row.skus[response.rows[key].id].size = response.rows[key].size;
+      //         row.skus[response.rows[key].id].quantity = response.rows[key].quantity;
+      //       }
+      //       // return row;
+      //       // console.log(row)
+      //       console.log('should have skus added', row);
+      //     })
+      //     .catch((err) => console.log('Error selecting skus'));
+      // });
+
+      // const addPhotoSkus = async () => {
+      //   for (let i = 0; i < stylesResult.rows.length; i++) {
+      //     let row = stylesResult.rows[i]
+      //     let styleID = row.id;
+      //     const photosResult = await pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
+      //       .then(async (response) => {
+      //         row.photos = (response.rows);
+      //       })
+      //       // .then(() => console.log('pls', styles))
+      //       .catch((err) => console.log('Error selecting photos'));
+      //     const skusResult = await pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
+      //       .then((response) => {
+      //         // console.log('skus result', response.rows);
+      //         row.skus = {};
+      //         for (key in response.rows) {
+      //           row.skus[response.rows[key].id] = {};
+      //           row.skus[response.rows[key].id].size = response.rows[key].size;
+      //           row.skus[response.rows[key].id].quantity = response.rows[key].quantity;
+      //         }
+      //         // return row;
+      //         // console.log(row)
+      //         // console.log('should have skus added', row);
+      //       })
+      //       .catch((err) => console.log('Error selecting skus'));
+      //   }
+      //   callback(null, stylesResult.rows);
+      // }
+      // addPhotoSkus();
+
       callback(null, stylesResult.rows);
     } catch (error) {
       callback(error, null);
@@ -56,3 +123,30 @@ module.exports = {
 };
 
 // const stylesResult = await pool.query("SELECT styles.*, photos.url, photos.thumbnail_url FROM styles INNER JOIN photos ON photos.styleID=styles.id WHERE productId=$1", [productID]);
+
+      // .then(async (res) => {
+      //   console.log('first time rows', res.rows)
+      //   let styleID = res.rows.id;
+      //   res.rows.forEach(async row => {
+      //     await pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
+      //       .then((response) => {
+      //         row.photos = (response.rows);
+      //       })
+      //       .catch((err) => console.log('Error selecting photos'));
+      //     await pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
+      //       .then((response) => {
+      //         // console.log('skus result', response.rows);
+      //         row.skus = {};
+      //         for (key in response.rows) {
+      //           row.skus[response.rows[key].id] = {};
+      //           row.skus[response.rows[key].id].size = response.rows[key].size;
+      //           row.skus[response.rows[key].id].quantity = response.rows[key].quantity;
+      //         }
+      //       })
+      //       .catch((err) => console.log('Error selecting skus'));
+      //   })
+      //   console.log('first then', res.rows);
+      //   return res.rows;
+      // })
+      // .then((res) => console.log(res))
+      // .catch((err) => console.log(err))
