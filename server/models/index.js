b9347c3c385fa65a00a80fb70ce6d4508cdeb183
Curtis Wang <curtis.wang10@gmail.com>
2022-11-04 22:03:36 -0700

    finally made progress on the queries using joins and more complex queries. jsonb_agg and create object are interesting tools to try and implement

--- server/models/index.js
+++ server/models/index.js
@@ -4,19 +4,34 @@ const { pool } = require('../db/db.js');
 module.exports = {
   getRelated: async (productID, callback) => {
     try {
-      const relatedResult = await pool.query("SELECT related_product_id FROM related WHERE current_product_id=$1", [productID]);
-      const newRelated = [];
-      relatedResult.rows.forEach(row => newRelated.push(row.related_product_id));
-      callback(null, newRelated);
+      // const relatedResult = await pool.query("SELECT related_product_id FROM related WHERE current_product_id=$1", [productID]);
+      // const newRelated = [];
+      // relatedResult.rows.forEach(row => newRelated.push(row.related_product_id));
+      const relatedResult = await pool.query(`
+        SELECT
+          jsonb_agg(related_product_id) AS rps
+        FROM related
+        WHERE current_product_id=$1
+      `, [productID]);
+      callback(null, relatedResult.rows[0].rps);
     } catch (error) {
       callback(error, null);
     }
   },
   getProduct: async (productID, callback) => {
     try {
-      const productResult = await pool.query("SELECT * FROM product WHERE id=$1", [productID]);
-      const featuresResult = await pool.query("SELECT feature, value FROM features WHERE product_id=$1", [productID]);
-      productResult.rows[0].features = featuresResult.rows;
+      // const productResult = await pool.query("SELECT * FROM product WHERE id=$1", [productID]);
+      // const featuresResult = await pool.query("SELECT feature, value FROM features WHERE product_id=$1", [productID]);
+      // productResult.rows[0].features = featuresResult.rows;
+      const productResult = await pool.query(`
+        SELECT
+          product.*,
+          json_agg(json_build_object('feature', features.feature, 'value', features.value)) AS features
+        FROM product
+        LEFT JOIN features ON features.product_id=product.id
+        GROUP BY product.id, features.product_id
+        HAVING product_id=$1
+      `, [productID]);
       callback(null, productResult.rows);
     } catch (error) {
       callback(error, null);
@@ -24,96 +39,34 @@ module.exports = {
   },
   getProducts: async (resultsCount, callback) => {
     try {
-      if (resultsCount === undefined) {
-        const productsResult = await pool.query("SELECT * FROM product LIMIT 5");
-        callback(null, productsResult.rows);
-      } else {
-        const productsResult = await pool.query("SELECT * FROM product LIMIT $1", [resultsCount]);
-        callback(null, productsResult.rows);
-      }
+      const productsResult = await pool.query(`
+        SELECT
+          *
+        FROM product
+        LIMIT 5
+      `);
+      callback(null, productsResult.rows);
     } catch (error) {
       callback(error, null);
     }
   },
   getStyles: async (productID, callback) => {
     try {
-      const stylesResult = await pool.query("SELECT styles.*, jsonb_agg(to_jsonb(photos) - 'id' - 'styleid') AS photos FROM styles LEFT JOIN photos ON photos.styleid=styles.id WHERE productId=$1 GROUP BY styles.id", [productID])
-      console.log(stylesResult.rows);
-      // stylesResult.rows.forEach((row, i) => {
-      //   delete row.productid;
-      //   let styleID = row.id;
-      //   pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
-      //     .then(photos => {
-      //       row.photos = photos.rows;
-
-      //       pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
-      //         .then(skus => {
-      //           row.skus = {};
-      //           for (key in skus.rows) {
-      //             row.skus[skus.rows[key].id] = {};
-      //             row.skus[skus.rows[key].id].size = skus.rows[key].size;
-      //             row.skus[skus.rows[key].id].quantity = skus.rows[key].quantity;
-      //           }
-      //           console.log(stylesResult.rows);
-      //           // if (i === stylesResult.rows.length - 1) {
-      //           //   callback(null, stylesResult.rows);
-      //           // }
-      //         })
-      //     })
-      // })
-
-      // stylesResult.rows.forEach(async row => {
-      //   let styleID = row.id;
-      //   const photosResult = await pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
-      //     .then(async (response) => {
-      //       row.photos = (response.rows);
-      //     })
-      //     // .then(() => console.log('pls', styles))
-      //     .catch((err) => console.log('Error selecting photos'));
-      //   const skusResult = await pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
-      //     .then((response) => {
-      //       // console.log('skus result', response.rows);
-      //       row.skus = {};
-      //       for (key in response.rows) {
-      //         row.skus[response.rows[key].id] = {};
-      //         row.skus[response.rows[key].id].size = response.rows[key].size;
-      //         row.skus[response.rows[key].id].quantity = response.rows[key].quantity;
-      //       }
-      //       // return row;
-      //       // console.log(row)
-      //       console.log('should have skus added', row);
-      //     })
-      //     .catch((err) => console.log('Error selecting skus'));
-      // });
-
-      // const addPhotoSkus = async () => {
-      //   for (let i = 0; i < stylesResult.rows.length; i++) {
-      //     let row = stylesResult.rows[i]
-      //     let styleID = row.id;
-      //     const photosResult = await pool.query("SELECT url, thumbnail_url FROM photos WHERE styleID=$1", [styleID])
-      //       .then(async (response) => {
-      //         row.photos = (response.rows);
-      //       })
-      //       // .then(() => console.log('pls', styles))
-      //       .catch((err) => console.log('Error selecting photos'));
-      //     const skusResult = await pool.query("SELECT id, size, quantity FROM skus WHERE styleID=$1", [styleID])
-      //       .then((response) => {
-      //         // console.log('skus result', response.rows);
-      //         row.skus = {};
-      //         for (key in response.rows) {
-      //           row.skus[response.rows[key].id] = {};
-      //           row.skus[response.rows[key].id].size = response.rows[key].size;
-      //           row.skus[response.rows[key].id].quantity = response.rows[key].quantity;
-      //         }
-      //         // return row;
-      //         // console.log(row)
-      //         // console.log('should have skus added', row);
-      //       })
-      //       .catch((err) => console.log('Error selecting skus'));
-      //   }
-      //   callback(null, stylesResult.rows);
-      // }
-      // addPhotoSkus();
+      const stylesResult = await pool.query(
+        `SELECT
+          styles.id AS style_id,
+          styles.name,
+          styles.original_price,
+          styles.sale_price,
+          styles.default_style AS "default?",
+          jsonb_agg(distinct jsonb_build_object('url', photos.url, 'thumbnail_url', photos.thumbnail_url)) AS photos,
+          jsonb_object_agg(COALESCE(CAST(skus.id AS VARCHAR), 'null'), jsonb_build_object('quantity', skus.quantity, 'size', skus.size)) AS skus
+        FROM styles
+        LEFT JOIN photos ON photos.styleid=styles.id
+        LEFT JOIN skus ON skus.styleid=styles.id
+        GROUP BY styles.id
+        HAVING productId=$1`
+        , [productID])
 
       callback(null, stylesResult.rows);
     } catch (error) {
